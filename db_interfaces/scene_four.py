95e36be0378bc0250e26500b3311051f40a46fc1
zhijian <1594937473@qq.com>
2023-12-24 16:38:55 +0800

    接口修改

--- db_interfaces/scene_four.py
+++ db_interfaces/scene_four.py
@@ -2,5 +2,13 @@ from others import db
 import pandas as pd
 
 def query_admin():
-    df = pd.read_sql_query("select ip,user,identity from user_marking",db.engine)
+    df = pd.read_sql_query("select ip,user,identity,domain from user_marking",db.engine)
+    return df
+
+def query_urlinfo_by_ip(ip):
+    df = pd.read_sql_query(f"select * from ip_url_net where ip = {ip}", db.engine)
+    return df
+
+def query_url_marking():
+    df = pd.read_sql_query(f"select url,marking,domain from url_marking", db.engine)
     return df
--- views/scene_four.py
+++ views/scene_four.py
@@ -3,6 +3,8 @@ from db_interfaces.scene_four import *
 import re
 import json
 import time
+
+from models.Url_marking import Url_marking
 from models.User_marking import User_marking
 
 
@@ -15,12 +17,38 @@ def admin2list():
 
 @bp.route('/admin2modify',methods=["GET","POST"])
 def admin2modify():
-    request_data = request.args.to_dict()
+    request_data = request.get_json()
     ip = request_data.get('ip')
     identity = request_data.get('identity')
     print(ip,identity)
     user = User_marking.query.get(ip)
     user.identity = identity
+    db.session.commit()
+
+    return 'success'
+
+@bp.route('/ip2url', methods=["GET"])
+def ip2url():
+    request_data = request.args.to_dict()
+    ip = request_data.get('ip')
+    domain = request_data.get('domain')
+    data = query_urlinfo_by_ip(ip)
+    url_marking = query_url_marking()
+    data = data.merge(url_marking, on='url', how='left')
+    data['domain'] = domain
+    data.fillna('无', inplace=True)
+
+    return data.to_dict('records')
+
+@bp.route('/url_marking',methods=["GET","POST"])
+def url_marking():
+    request_data = request.get_json()
+    url = request_data.get['url']
+    domain = request_data.get['domain']
+    marking = request_data.get['marking']
+    print(url,domain,marking)
+    url_mark = Url_marking(url,domain,marking)
+    db.session.merge(url_mark)
     db.session.commit()
 
     return 'success'
